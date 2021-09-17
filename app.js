cbe2e39c74e6e21cbac23efd6f8acc80ce74de29
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-09-17 19:14:56 -0300

    Listando as despesas no consulta
    Inserindo dados na tabela

--- app.js
+++ app.js
@@ -119,6 +119,34 @@ function carregaListaDespesas() {
     let despesas = Array()
     despesas = bd.recuperarTodosRegistros()
 
-    console.log(despesas)
+    let listaDespesas = document.getElementById('listaDespesas')
+
+    //percorrer o array despesas, listando cada despesa de forma dinâmica
+    despesas.forEach(function (d) {
+
+        //criando a linha (tr)
+        let linha = listaDespesas.insertRow()
+
+        //criar as colunas (td)
+        linha.insertCell(0).innerHTML = ` ${d.dia}/${d.mes}/${d.ano}`
+
+        //Ajustar o tipo
+        switch (d.tipo) {
+            case '1': d.tipo = 'Alimentação'
+                break;
+            case '2': d.tipo = 'Educação'
+                break;
+            case '3': d.tipo = 'Lazer'
+                break;
+            case '4': d.tipo = 'Saúde'
+                break;
+            case '5': d.tipo = 'Transporte'
+                break;
+        }
+        linha.insertCell(1).innerHTML = d.tipo
+        linha.insertCell(2).innerHTML = d.descricao
+        linha.insertCell(3).innerHTML = d.valor
+
+    })
 }
 
